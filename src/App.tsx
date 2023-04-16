88bcb7559680c94d285e13d869736ef516f69236
Ron Gorai <ron.gorai2020@gmail.com>
2023-04-16 18:43:50 -0400

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,109 @@
+// import FormGenerator from 'complex-form-generator'
+import { useRef, useState } from 'react'
+import JSONPretty from 'react-json-pretty'
 import FormGenerator from './complex-form-generator/src/FormGenerator'
 
+const TEST_SEED1: Seed = {
+  title: '',
+  subtitle: '',
+  disableOnSubmit: false,
+  class: '',
+  actions: [
+    {
+      label: '',
+      params: '',
+      disableOnSubmit: false,
+      type: {
+        $useSelectOptions: [
+          {
+            _option: 'button',
+          },
+          {
+            _option: 'link',
+            _assocPayload: {
+              disableForm: false,
+              url: '',
+              target: {
+                $useSelectOptions: [
+                  { _option: '_blank' },
+                  { _option: '_self' },
+                  { _option: '_parent' },
+                ],
+              },
+            },
+          },
+        ],
+      },
+    },
+  ],
+  inputs: [
+    {
+      icon: { sfIcon: '', src: '', alt: '' },
+      label: '',
+      placeholder: '',
+      value: '',
+      fApi: '',
+      validateOn: {
+        $useSelectOptions: [{ _option: 'blur' }, { _option: 'input' }],
+      },
+      isRequired: false,
+      minLength: '',
+      maxLength: '',
+    },
+  ],
+  submission: { server: '*', browser: '*' },
+}
+
+const TEST_SEED: Seed = {
+  title: '',
+  subtitle: '',
+  disableOnSubmit: false,
+  class: '',
+  actions: [
+    {
+      label: '',
+      params: '',
+      disableOnSubmit: false,
+      type: {
+        $useSelectOptions: [
+          {
+            _option: 'button',
+          },
+          {
+            _option: 'link',
+            _assocPayload: {
+              disableForm: false,
+              url: '',
+              target: {
+                $useSelectOptions: [
+                  { _option: '_blank' },
+                  { _option: '_self' },
+                  { _option: '_parent' },
+                ],
+              },
+            },
+          },
+        ],
+      },
+    },
+  ],
+}
+
 const App = () => {
+  const [payload, setPayload] = useState({})
+
   return (
     <div className="App">
       <div className="component-wrapper">
-        <FormGenerator />
+        <div style={{ width: '20em' }}>
+          <FormGenerator
+            originalSeed={useRef(TEST_SEED)}
+            usePrimarySeed={useState(TEST_SEED)}
+            setPayload={setPayload}
+          />
+        </div>
+
+        <JSONPretty data={payload} />
       </div>
     </div>
   )
